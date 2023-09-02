c40e8dd9d3a97be73bf4dedc3ba1bb2a180c1608
Oleg Maizer <zippooleg3@gmail.com>
2023-09-02 12:44:30 +0300

    1.2

--- part1/courseinfo/src/Content.jsx
+++ part1/courseinfo/src/Content.jsx
@@ -1,9 +1,11 @@
+import Part from "./Part"
+
 const Content = (props) => {
   return (
     <>
-      <p>{props.part1.title} {props.part1.exercises}</p>
-      <p>{props.part2.title} {props.part2.exercises}</p>
-      <p>{props.part3.title} {props.part3.exercises}</p>
+      <Part content={props.part1}/>
+      <Part content={props.part2}/>
+      <Part content={props.part3}/>
     </>
   )
 }
---
+++ part1/courseinfo/src/Part.jsx
@@ -0,0 +1,9 @@
+const Part = (props) => {
+  return (
+    <>
+      <p>{props.content.title} {props.content.exercises}</p>
+    </>
+  )
+}
+
+export default Part
