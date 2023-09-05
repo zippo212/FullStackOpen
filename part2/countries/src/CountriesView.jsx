c34738cbf86adf86c3225eb32b383627f3091b8f
Oleg Maizer <zippooleg3@gmail.com>
2023-09-05 20:24:13 +0300

    2.19

--- part2/countries/src/CountriesView.jsx
+++ part2/countries/src/CountriesView.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react"
 
 const Country = ({country}) => {
   return (
@@ -11,20 +12,30 @@ const Country = ({country}) => {
       <ul>
         {Object.values(country.languages).map(lang => <li key={lang}>{lang}</li>)}
       </ul>
-      <img src={country.flags.png} alt={country.flags.alt} />
+      <img src={country.flags.png} alt={country.flags.alt} style={{border: 'solid'}}/>
     </div>
   )
 }
 
 const CountriesView = ({countries}) => {
-
+  const [showCountry, setShowCountry] = useState([])
   if (countries.length === 1) return <Country country={countries[0]}/>  
+
+  function handleClick(name) {
+    if (!showCountry.includes(name)) {
+      setShowCountry(showCountry.concat(name))
+    } else setShowCountry(showCountry.filter(c => c !== name))
+  }
   
   return (
     <div>
       {countries.map(country => 
         <div key={country.name.common} style={{marginBottom: 2 , fontSize: 18}}>
           {country.name.common}
+          <button onClick={() => handleClick(country.name.common)}>
+            {showCountry.includes(country.name.common) ? 'hide' : 'show'}
+          </button>
+          {showCountry.includes(country.name.common) && <Country country={country}/>}
         </div> 
       )}
     </div>
