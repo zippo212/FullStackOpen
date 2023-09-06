22b589cb33702edeaa5956b340cc5a2bd6f5990e
Oleg Maizer <zippooleg3@gmail.com>
2023-09-06 22:24:33 +0300

    3.7

--- part3/index.js
+++ part3/index.js
@@ -1,6 +1,10 @@
 const express = require("express")
+const morgan = require("morgan")
+
 const app = express()
+
 app.use(express.json())
+app.use(morgan("tiny"))
 
 let phoneBook = [
   {
@@ -58,6 +62,7 @@ const generateId = () => {
 }
 
 app.post("/api/persons", (req, res) => {
+  console.log(req.body)
   const body = req.body
 
   if (body.name && body.number) {
