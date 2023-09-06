a993b602b85f5f32e3a076af9a2432be456f5da0
Oleg Maizer <zippooleg3@gmail.com>
2023-09-06 19:32:12 +0300

    3.2

--- part3/index.js
+++ part3/index.js
@@ -28,6 +28,13 @@ app.get("/api/persons", (req, res) => {
   res.json(data)
 })
 
+app.get("/info", (req, res) => {
+  res.send(`
+      <p>Phonebook has info for ${data.length} people</p>
+      <p>${Date()}</p>
+    `)
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
   console.log("server listening on port", PORT)
