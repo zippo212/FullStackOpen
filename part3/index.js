9089d2b9e0b682dbb18c8c8d2cb3fd8f41a5b473
Oleg Maizer <zippooleg3@gmail.com>
2023-09-06 22:43:00 +0300

    3.8

--- part3/index.js
+++ part3/index.js
@@ -4,7 +4,10 @@ const morgan = require("morgan")
 const app = express()
 
 app.use(express.json())
-app.use(morgan("tiny"))
+
+morgan.token("body", (req, res) => JSON.stringify(req.body))
+
+app.use(morgan(":method :url :status :res[content-length] - :response-time ms :body"))
 
 let phoneBook = [
   {
