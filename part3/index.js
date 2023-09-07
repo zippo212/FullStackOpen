cb05d7280ecabc283a74aa65cc7a46dc3ca4b598
Oleg Maizer <zippooleg3@gmail.com>
2023-09-07 22:23:22 +0300

    3.10

--- part3/index.js
+++ part3/index.js
@@ -85,7 +85,7 @@ app.post("/api/persons", (req, res) => {
   }
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log("server listening on port", PORT)
 })
