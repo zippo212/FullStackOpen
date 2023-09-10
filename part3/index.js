3a80543b2bd3d25b9a1b63edb4f575fb89d0eaf0
Oleg Maizer <zippooleg3@gmail.com>
2023-09-11 00:28:45 +0300

    3.22

--- part3/index.js
+++ part3/index.js
@@ -9,7 +9,7 @@ app.use(cors())
 
 app.use(express.static("dist"))
 app.use(express.json())
-morgan.token("body", (req, res) => JSON.stringify(req.body))
+morgan.token("body", (req) => JSON.stringify(req.body))
 app.use(morgan(":method :url :status :res[content-length] - :response-time ms :body"))
 
 app.get("/info", (req, res, next) => {
@@ -43,7 +43,7 @@ app.get("/api/persons/:id", (req, res, next) => {
 
 app.delete("/api/persons/:id", (req, res, next) => {
   Person.findByIdAndRemove(req.params.id)
-    .then((result) => res.status(204).end())
+    .then(() => res.status(204).end())
     .catch((error) => next(error))
 })
 
@@ -57,38 +57,28 @@ app.post("/api/persons", (req, res, next) => {
     .then((result) => {
       res.status(201).json(result)
     })
-    .catch((error) => {
-      next(error)
-    })
+    .catch((error) => next(error))
 })
 
 app.put("/api/persons/:id", (req, res, next) => {
-  const body = req.body
+  const { name, number } = req.body
 
-  const newEntry = {
-    name: body.name,
-    number: body.number,
-  }
+  const newEntry = { name, number }
 
   Person.findByIdAndUpdate(req.params.id, newEntry, { new: true, runValidators: true, context: "query" })
     .then((updatedPerson) => res.json(updatedPerson))
-    .catch((error) => {
-      next(error)
-    })
+    .catch((error) => next(error))
 })
 
 const errorHandler = (error, request, response, next) => {
   console.error(error.message)
-
   if (error.name === "CastError") {
     return response.status(400).send({ error: "malformatted id" })
   } else if (error.name === "ValidationError") {
     return response.status(400).json({ error: error.message })
   }
-
   next(error)
 }
-// this has to be the last loaded middleware.
 app.use(errorHandler)
 
 const PORT = process.env.PORT
--- part3/models/person.js
+++ part3/models/person.js
@@ -5,7 +5,7 @@ mongoose.set("strictQuery", false)
 
 mongoose
   .connect(url)
-  .then((result) => {
+  .then(() => {
     console.log("connected to MongoDB")
   })
   .catch((error) => {
