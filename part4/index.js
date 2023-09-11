7c1f44b89f9b3311acd851f9e150513391ddfd85
Oleg Maizer <zippooleg3@gmail.com>
2023-09-11 18:43:33 +0300

    Added utils for config and logging

--- part4/index.js
+++ part4/index.js
@@ -1,22 +1,23 @@
-require("dotenv").config()
+const config = require("./utils/config")
+const { info, error } = require("./utils/logger")
 const express = require("express")
 const app = express()
 const cors = require("cors")
 const mongoose = require("mongoose")
 const blogsRouter = require("./controllers/blogs")
 
-const mongoUrl = process.env.MONGODB_URI
+const mongoUrl = config.MONGODB_URI
 
 mongoose
   .connect(mongoUrl)
-  .then(() => console.log("connected to MongoDB"))
-  .catch((err) => console.error("error connecting to MongoDB:", error.message))
+  .then(() => info("connected to MongoDB"))
+  .catch((err) => error("error connecting to MongoDB:", error.message))
 
 app.use(cors())
 app.use(express.json())
 
 app.use("/api/blogs", blogsRouter)
 
-app.listen(process.env.PORT, () => {
-  console.log(`Server running on port ${process.env.PORT}`)
+app.listen(config.PORT, () => {
+  info(`Server running on port ${config.PORT}`)
 })
