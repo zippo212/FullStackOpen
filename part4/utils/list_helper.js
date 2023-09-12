298250e725bdd8cce0d06d7b1ff9c4441a2ce4df
Oleg Maizer <zippooleg3@gmail.com>
2023-09-12 22:35:26 +0300

    4.7

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -1,3 +1,5 @@
+const { countBy, maxBy, toPairs, groupBy, mapValues, sumBy } = require("lodash")
+
 const dummy = (blogs) => {
   return 1
 }
@@ -16,8 +18,31 @@ const favoriteBlog = (blogs) => {
   }, {})
 }
 
+const mostBlogs = (blogs) => {
+  if (blogs.length === 0) return {}
+  const authorBlogsCount = countBy(blogs, (blog) => blog.author)
+  const mostBlogsAuthor = maxBy(toPairs(authorBlogsCount), ([, blogsCount]) => blogsCount)
+  return {
+    author: mostBlogsAuthor[0],
+    blogs: mostBlogsAuthor[1],
+  }
+}
+
+const mostLikes = (blogs) => {
+  if (blogs.length === 0) return {}
+  const authorBlogsGroup = groupBy(blogs, "author")
+  const authorTotalLikes = mapValues(authorBlogsGroup, (authorBlogs) => sumBy(authorBlogs, "likes"))
+  const mostBlogsLikesAuthor = maxBy(toPairs(authorTotalLikes), ([, likesCount]) => likesCount)
+  return {
+    author: mostBlogsLikesAuthor[0],
+    likes: mostBlogsLikesAuthor[1],
+  }
+}
+
 module.exports = {
   dummy,
   totalLikes,
   favoriteBlog,
+  mostBlogs,
+  mostLikes,
 }
