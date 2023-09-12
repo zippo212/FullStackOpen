1ab89136838317be8ce11f1f3e3eef5b33c7374d
Oleg Maizer <zippooleg3@gmail.com>
2023-09-12 21:09:19 +0300

    4.5

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -2,6 +2,22 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+  return blogs.reduce((total, current) => {
+    return (total = total + current.likes)
+  }, 0)
+}
+
+const favoriteBlog = (blogs) => {
+  return blogs.reduce((max, current) => {
+    console.log(max, current)
+    if (Object.keys(max).length === 0) return current
+    return current.likes > max.likes ? current : max
+  }, {})
+}
+
 module.exports = {
   dummy,
+  totalLikes,
+  favoriteBlog,
 }
