73a93831e7f54f7b885819c2d43ab51d6c554031
Oleg Maizer <zippooleg3@gmail.com>
2023-09-18 22:53:55 +0300

    5.4

--- part5/src/App.jsx
+++ part5/src/App.jsx
@@ -4,10 +4,33 @@ import blogService from './services/blogs'
 import LoginForm from './components/LoginForm'
 import NewBlogForm from './components/NewBlogForm'
 
+const Notification = ({success, error}) => {
+  const message = success || error
+  if (!message) return  
+  
+  const color = success ? 'green' : 'red';
+  const notificationStyle = {
+    color: `${color}`,
+    padding: 10,
+    fontSize: 20,
+    fontWeight: 700,
+    background: 'lightgrey',
+    marginBottom: 10,
+    borderRadius: 8,
+    border: `medium dashed ${color}`
+  }
+  return (
+    <div style={notificationStyle}>
+      {message}
+    </div>
+  )
+}
+
 const App = () => {
   const [user, setUser] = useState(null)
   const [blogs, setBlogs] = useState([])
-  const [error, setError] = useState(null)
+  const [successMessage, setSuccessMessage] = useState(null)
+  const [errorMessage, setErrorMessage] = useState(null)
 
   useEffect(() => {
     blogService.getAll().then((blogs) => setBlogs(blogs))
@@ -26,14 +49,19 @@ const App = () => {
     localStorage.setItem('loggedBlogAppUser',JSON.stringify(user))
     blogService.setUpToken(user.token)
     setUser(user)
+    setSuccessMessage(`${user.name} logged in successfully`)
+    setTimeout(() => setSuccessMessage(null), 3000)
   }
 
   const updateBlogs = (newBlog) => {
     setBlogs(blogs.concat(newBlog))
+    setSuccessMessage(`a new blog ${newBlog.title} by ${newBlog.author} added`)
+    setTimeout(() => setSuccessMessage(null), 3000)
   }
 
   const handleError = (err) => {
-    setError(err)
+    setErrorMessage(err)
+    setTimeout(() => setErrorMessage(null), 3000)
   }
 
   const handleLogOut = () => {
@@ -45,6 +73,7 @@ const App = () => {
   return (
     <div>
       <h2>{user ? "Blogs" : "Login to application"}</h2>
+      <Notification success={successMessage} error={errorMessage}/> 
       {!user 
         ? <LoginForm updateUser={updateUser} handleError={handleError}/>
         : <> 
