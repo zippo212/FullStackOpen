b2963c6d1bdb84d0b5830db71ad4adbd5364ed01
Oleg Maizer <zippooleg3@gmail.com>
2023-09-19 19:15:13 +0300

    5.7

--- part5/src/components/Blog.jsx
+++ part5/src/components/Blog.jsx
@@ -1,9 +1,32 @@
+import { useState } from "react"
+
 const Blog = ({ blog }) => {
+  const [isVisible, setIsVisible] = useState(false)
+  const buttonContent = isVisible ? "hide" : "show"
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
   return (
-    <div>
-    {blog.title} {blog.author}
-  </div>  
-  )
-}
+    <div style={blogStyle}>
+      <div>
+        {blog.title} {blog.author}
+        <button onClick={() => setIsVisible(!isVisible)}>{buttonContent}</button>
+      </div>
+      <div style={{display: isVisible ? "" : "none"}}>
+        <div>{blog.url}</div>
+        <div>
+          {blog.likes}
+          <button>like</button>
+        </div>
+        <div>{blog.user.name}</div>
+      </div>
+    </div>
+)}
 
 export default Blog
