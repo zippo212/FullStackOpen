0620d1a0ff619439e6c91e006a069fdb2be3d79c
Oleg Maizer <zippooleg3@gmail.com>
2023-09-23 21:58:16 +0300

    6.3

---
+++ part6/anecdotes/src/App.jsx
@@ -0,0 +1,35 @@
+import { useSelector, useDispatch } from 'react-redux'
+import { vote } from './reducers/anecdoteReducer'
+
+const App = () => {
+  const anecdotes = useSelector((state) => state)
+  const dispatch = useDispatch()
+
+  const handleVote = (id) => {
+    dispatch(vote(id))
+  }
+
+  return (
+    <div>
+      <h2>Anecdotes</h2>
+      {anecdotes.map(anecdote =>
+        <div key={anecdote.id}>
+          <div>
+            {anecdote.content}
+          </div>
+          <div>
+            has {anecdote.votes}
+            <button onClick={() => handleVote(anecdote.id)}>vote</button>
+          </div>
+        </div>
+      )}
+      <h2>create new</h2>
+      <form>
+        <div><input /></div>
+        <button>create</button>
+      </form>
+    </div>
+  )
+}
+
+export default App
