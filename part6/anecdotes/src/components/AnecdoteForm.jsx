4afc6c64d73a00fc8171c2967f394d8a01308804
Oleg Maizer <zippooleg3@gmail.com>
2023-09-23 22:24:03 +0300

    6.7

---
+++ part6/anecdotes/src/components/AnecdoteForm.jsx
@@ -0,0 +1,27 @@
+import { useDispatch } from 'react-redux'
+import { addAnecdote } from '../reducers/anecdoteReducer'
+
+const AnecdoteForm = () => {
+  const dispatch = useDispatch()
+
+  const handleAdd = (e) => {
+    e.preventDefault()
+    const content = e.target.anecdote.value
+    e.target.anecdote.value = ''
+    dispatch(addAnecdote(content))
+  }
+
+  return (
+    <>
+      <h2>create new</h2>
+      <form  onSubmit={handleAdd}>
+        <div>
+          <input name='anecdote' />
+        </div>
+        <button>create</button>
+      </form>
+    </>
+  )
+}
+
+export default AnecdoteForm
