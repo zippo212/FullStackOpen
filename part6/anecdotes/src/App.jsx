54846602e50a520a3e9d0712ef814cbbca2789fe
Oleg Maizer <zippooleg3@gmail.com>
2023-09-25 18:06:50 +0300

    6.16

--- part6/anecdotes/src/App.jsx
+++ part6/anecdotes/src/App.jsx
@@ -4,15 +4,13 @@ import AnecdoteForm from './components/AnecdoteForm'
 import AnecdoteList from './components/AnecdoteList'
 import Filter from './components/Filter'
 import Notification from './components/Notification'
-import anecdoteService from './services/anecdoteService'
-import { setAnecdotes } from './reducers/anecdoteReducer'
+import { initializeAnecdotes } from './reducers/anecdoteReducer'
 
 const App = () => {
   const dispatch = useDispatch()
   
   useEffect(() => {
-    anecdoteService
-      .getAll().then((data) => dispatch(setAnecdotes(data)))
+    dispatch(initializeAnecdotes())
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
 
