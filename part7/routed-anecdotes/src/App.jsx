f0e02394230c5cb23a9e341cacdec12bd086e550
Oleg Maizer <zippooleg3@gmail.com>
2023-09-28 14:35:18 +0300

    7.4

--- part7/routed-anecdotes/src/App.jsx
+++ part7/routed-anecdotes/src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import { useField } from './hooks/index.js'
 import {
   Routes,
   Route,
@@ -72,9 +73,9 @@ const Notification = ({notification}) => (
 )
 
 const CreateNew = ({addNew,setUpNotification}) => {
-  const [content, setContent] = useState('')
-  const [author, setAuthor] = useState('')
-  const [info, setInfo] = useState('')
+  const content = useField('text')
+  const author = useField('text')
+  const info = useField('text')
   
   const navigate = useNavigate()
 
@@ -96,15 +97,15 @@ const CreateNew = ({addNew,setUpNotification}) => {
       <form onSubmit={handleSubmit}>
         <div>
           content
-          <input name='content' value={content} onChange={(e) => setContent(e.target.value)} />
+          <input name='content' {...content} />
         </div>
         <div>
           author
-          <input name='author' value={author} onChange={(e) => setAuthor(e.target.value)} />
+          <input name='author' {...author} />
         </div>
         <div>
           url for more info
-          <input name='info' value={info} onChange={(e)=> setInfo(e.target.value)} />
+          <input name='info' {...info} />
         </div>
         <button>create</button>
       </form>
